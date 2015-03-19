461f1fee6791d039fdeb5ff7d57a2895b2b566eb
Matthew Vincent <matt@averails.com>
2015-03-19 01:37:37 +0100

    adding bbcode parser

--- lib/sketchfably.rb
+++ lib/sketchfably.rb
@@ -10,6 +10,17 @@ module Sketchfably
     return models
   end
 
+  def self.get_model_from_bbcode(bbcode)
+    ## [sketchfab]55ea0aed9bfd462593f006ea8c4aade0[/sketchfab]
+      #[url=https://sketchfab.com/models/55ea0aed9bfd462593f006ea8c4aade0]The Lion of Mosul[/url] by [url=https://sketchfab.com/neshmi]neshmi[/url] on [url=https://sketchfab.com]Sketchfab[/url]
+    model = {}
+    bbcode.match(/\[sketchfab\](.*)\[\/sketchfab\]\s\[url.*\](.*)\[\/url\].*\[url.*\](.*)\[\/url\].*\[url.*\](.*)\[\/url\]/)
+    model[:id] = $1
+    model[:name] = $2
+    model[:username] = $3
+    return model
+  end
+
   def self.get_html_for_model(model_hash)
     html = <<-eol
 <iframe width="640" height="480" src="https://sketchfab.com/models/#{model_hash[:id]}/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
--- spec/sketchfably_spec.rb
+++ spec/sketchfably_spec.rb
@@ -28,4 +28,26 @@ describe Sketchfably do
       expect(Sketchfably.get_html_for_model model).to eq(html_template)
     end
   end
+
+  describe "bbcode" do
+    def bbcode
+      File.read(File.join(File.dirname(__FILE__),"fixtures","bbcode"))
+    end
+
+    def model
+      Sketchfably.get_models_by_tag("projectmosul").first
+    end
+
+    def html_template
+      File.read(File.join(File.dirname(__FILE__),"fixtures","embed_code"))
+    end
+
+    it "should parse bbcode and abstract id, username, and model name" do
+      expect(Sketchfably.get_model_from_bbcode(bbcode)).to eq(model)
+    end
+
+    it "should get html template from bbcode" do
+      expect(Sketchfably.get_html_for_model Sketchfably.get_model_from_bbcode(bbcode)).to eq(html_template)
+    end
+  end
 end
